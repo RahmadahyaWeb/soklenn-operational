de7bbc7d5d5f099a74296fbcc524eff1e84b0782
rahmadahyaWeb <thelastpc24@gmail.com>
2026-06-24 23:06:17 +0800

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,26 +21,8 @@ class DatabaseSeeder extends Seeder
 
         MembershipReward::insert([
             [
-                'name' => '10%',
-                'required_stamp' => 3,
-                'reward_type' => 'discount_percentage',
-                'reward_value' => 10,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-
-            [
-                'name' => '10K',
-                'required_stamp' => 5,
-                'reward_type' => 'discount_fixed',
-                'reward_value' => 10000,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-
-            [
                 'name' => '15%',
-                'required_stamp' => 7,
+                'required_stamp' => 3,
                 'reward_type' => 'discount_percentage',
                 'reward_value' => 15,
                 'created_at' => now(),
@@ -48,10 +30,28 @@ class DatabaseSeeder extends Seeder
             ],
 
             [
-                'name' => '20K',
-                'required_stamp' => 10,
+                'name' => '20k',
+                'required_stamp' => 5,
                 'reward_type' => 'discount_fixed',
                 'reward_value' => 20000,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            [
+                'name' => '20%',
+                'required_stamp' => 7,
+                'reward_type' => 'discount_percentage',
+                'reward_value' => 20,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            [
+                'name' => '100%',
+                'required_stamp' => 10,
+                'reward_type' => 'discount_percentage',
+                'reward_value' => 100,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
@@ -61,6 +61,15 @@ class DatabaseSeeder extends Seeder
                 'required_stamp' => 12,
                 'reward_type' => 'discount_percentage',
                 'reward_value' => 20,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            [
+                'name' => '100%',
+                'required_stamp' => 14,
+                'reward_type' => 'discount_percentage',
+                'reward_value' => 100,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
